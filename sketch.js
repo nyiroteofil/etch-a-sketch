d225620eea202c7bc4814f0824ec1c9308654c1f
NyirőTeofil <portrider01234@gmail.com>
2022-07-10 18:53:30 +0200

    Etch-a-sketch project
    Fully impemented the clear button and added an
    adjustable grid feature
    
    2022.07.10 18:55

--- sketch.js
+++ sketch.js
@@ -1,34 +1,91 @@
 const canvas = document.querySelector('.canvas');
 const slider = document.querySelector('.slider');
 const sliderCaption = document.querySelector('.size-count')
+const clearButton = document.querySelector('.clear');
+const gridCheck = document.querySelector('.gridCheck');
+let rainbowON = false;
+
+    clearButton.addEventListener('click', () => {
+        let tiles = document.querySelectorAll('.tile')
+
+        tiles.forEach((n) => {
+            if (gridCheck.checked === true){
+                n.style = 'background-color: rgb(215, 218, 189); border: 1px black solid';
+            }
+            else {
+                n.style = 'background-color: rgb(215, 218, 189);';
+            }            
+            
+
+        });
+    });
 
     function deleteAllChild(node) {
+        
         while (node.firstChild) {
             node.removeChild(node.firstChild);
         }
+
     }
+
+    function rainbowColorHex () {
+
+        let colorCode = '';
+
+        for (let i = 0; i < 3; i++) {
+            let num = (Math.floor(Math.random() * (0 + 255)));
+            colorCode += num + ', ';
+        } 
+
+        colorCode.slice(13, 2);
+
+        return `rgb(${colorCode})`; 
+        
+    }
+
+/*-------------------- Setting up the basic grid and listeners --------------------*/
 
     canvas.style.gridTemplateColumns = `repeat(${slider.value}, 1fr)`;
     canvas.style.gridTemplateRows = `repeat(${slider.value}, 1fr)`;
 
     for (let i = 0; i < slider.value * slider.value; i++) {
+
         let tile = document.createElement('div');
-        tile.style.border ='solid black 1px';
         tile.classList.add('tile');
         canvas.appendChild(tile);
+    
     }
 
+    console.log(rainbowColorHex());
 
-    /*Event listener before the slider is being set*/
-    let tiles1 = document.querySelectorAll('.tile')
+    /*Event listener for the tiles before the slider is being set*/
+    let tiles = document.querySelectorAll('.tile')
 
-    tiles1.forEach((t) => {
-        t.addEventListener('mouseover', (e) => {
+    tiles.forEach((t) => {
+        t.addEventListener('mouseenter', (e) => {
+            if (rainbowON === false) {
                 e.target.style = 'background-color: black';
+            }        
+
         })
     })
 
-    slider.oninput = () => {
+    gridCheck.oninput = () => {
+        tiles.forEach((n) => {
+            if (gridCheck.checked === true) {
+                n.style = 'background-color: rgb(215, 218, 189); border: 1px black solid';
+            }
+            else {
+                n.style = 'background-color: rgb(215, 218, 189);'
+            }
+        });
+    }
+    
+
+/*-------------------- Setting up the basic grid and listeners --------------------*/
+/*-------------- Setting up the slider configured grid and listeners --------------*/
+
+slider.oninput = () => {
 
     
     deleteAllChild(canvas);
@@ -40,18 +97,38 @@ const sliderCaption = document.querySelector('.size-count')
 
     for (let i = 0; i < slider.value * slider.value; i++) {
         let tile = document.createElement('div');
-        tile.style.border ='solid black 1px';
         tile.classList.add('tile');
+
+        if(gridCheck.checked === true) {
+            tile.style = 'border: 1px solid black';
+        }
+
         canvas.appendChild(tile);
     }
 
     let tiles = document.querySelectorAll('.tile')
 
     tiles.forEach((t) => {
-        t.addEventListener('mouseover', (e) => {
+        t.addEventListener('mouseenter', (e) => {
+
+            if (rainbowON === false) {
                 e.target.style = 'background-color: black';
+            }
+
         })
     })
+
+    gridCheck.oninput = () => {
+        tiles.forEach((n) => {
+            if (gridCheck.checked === true) {
+                n.style = 'background-color: rgb(215, 218, 189); border: 1px black solid';
+            }
+            else {
+                n.style = 'background-color: rgb(215, 218, 189);'
+            }
+        });
+    }
+   
 }
 
 
